3706038eba19a9cb5f4ea26f251642c75b5e8c84
KeyboardFire <andy@keyboardfire.com>
2018-01-10 17:40:30 -0600

    add !bash and support non-supergroups

--- kipfa.py
+++ kipfa.py
@@ -11,6 +11,7 @@ from pyrogram.api import types, functions
 import speech_recognition as sr
 
 import requests
+from bs4 import BeautifulSoup
 
 import data
 sys.path.insert(0, './steno-keyboard-generator')
@@ -24,6 +25,12 @@ def xtoi(s):
 def getuotd():
     r = requests.get('https://lichess.org/training/daily')
     return r.text[r.text.find('og:title')+33:].split()[0]
+
+def chat_id(msg):
+    if isinstance(msg, types.Message):
+        return msg.to_id.channel_id
+    else:
+        return msg.chat_id
 
 class Perm:
 
@@ -50,6 +57,7 @@ class Bot:
             'js':       (self.cmd_js,       Perm([], [])),
             'steno':    (self.cmd_steno,    Perm([], [])),
             'expand':   (self.cmd_expand,   Perm([], [])),
+            'bash':     (self.cmd_bash,     Perm([], [])),
             'restart':  (self.cmd_restart,  Perm([212594557], []))
         }
         self.uotd = getuotd()
@@ -80,18 +88,18 @@ class Bot:
         self.reply(msg, os.popen("""node -e 'var Sandbox = require("./node_modules/sandbox"), s = new Sandbox(); s.options.timeout = 2000; s.run("{}", function(x) {{ console.log(x.result == "TimeoutError" ? "2 second timeout reached." : x.result); }});'""".format(args.replace('\\', '\\\\').replace("'", "'\\''").replace('"', '\\"'))).read())
 
     def cmd_steno(self, msg, args):
-        if re.fullmatch(r'S?T?K?P?W?H?R?A?O?\*?-?E?U?F?R?P?B?L?G?T?S?D?Z?', args):
-            m = re.search(r'[AO*\-EU]+', args)
-            if m:
-                dups = 'SPTR'
-                keyboard.draw_keyboard_to_png(
-                        [s+('-' if s in dups else '') for s in args[:m.start()]] +
-                        [s for s in m.group() if s != '-'] +
-                        [('-' if s in dups else '')+s for s in args[m.end():]],
-                        'tmp.png')
-                self.reply_photo(msg, 'tmp.png')
-            else:
-                self.reply(msg, 'Invalid steno.')
+        if args is None:
+            self.reply(msg, 'Please specify a steno string.')
+            return
+        m = re.search(r'[AO*\-EU]+', args)
+        if re.fullmatch(r'S?T?K?P?W?H?R?A?O?\*?-?E?U?F?R?P?B?L?G?T?S?D?Z?', args) and m:
+            dups = 'SPTR'
+            keyboard.draw_keyboard_to_png(
+                    [s+('-' if s in dups else '') for s in args[:m.start()]] +
+                    [s for s in m.group() if s != '-'] +
+                    [('-' if s in dups else '')+s for s in args[m.end():]],
+                    'tmp.png')
+            self.reply_photo(msg, 'tmp.png')
         else:
             self.reply(msg, 'Invalid steno.')
 
@@ -104,6 +112,11 @@ class Bot:
         else:
             self.reply(msg, ' '.join([os.popen("grep '^{}[a-z]*$' /usr/share/dict/words | shuf -n1".format(ch)).read().strip() for ch in args]))
 
+    def cmd_bash(self, msg, args):
+        # quote = BeautifulSoup(requests.get('http://bash.org/?random1').text, 'html.parser').find('p', class_='qt').text
+        quote = max(BeautifulSoup(requests.get('http://bash.org/?random1').text, 'html.parser').find_all('p', class_='quote'), key=lambda x: int(x.font.text)).next_sibling.text
+        self.reply(msg, '```\n{}\n```'.format(quote))
+
     def cmd_restart(self, msg, args):
         self.reply(msg, 'restarting...')
         self.client.stop()
@@ -111,17 +124,17 @@ class Bot:
 
     def checkuotd(self):
         newuotd = getuotd()
-        if self.uotd != newuotd:
+        if newuotd.isdecimal() and self.uotd != newuotd:
             self.uotd = newuotd
             client.send_message(1059322065, 'obtw new uotd')
 
     def reply(self, msg, txt):
         print(txt)
-        client.send_message(msg.to_id.channel_id, txt, reply_to_msg_id=msg.id)
+        client.send_message(chat_id(msg), txt, reply_to_msg_id=msg.id)
 
     def reply_photo(self, msg, path):
         print(path)
-        client.send_photo(msg.to_id.channel_id, path#, reply_to_msg_id=msg.id)
+        client.send_photo(chat_id(msg), path#, reply_to_msg_id=msg.id)
                 )
 
     def process_message(self, msg):
@@ -164,6 +177,9 @@ class Bot:
             msg = update.message
             print(msg)
             self.process_message(msg)
+        elif isinstance(update, types.UpdateShortChatMessage):
+            print(update)
+            self.process_message(update)
 
 client = Client('meemerino')
 bot = Bot(client)
@@ -171,5 +187,8 @@ client.set_update_handler(bot.callback)
 client.start()
 
 while True:
-    time.sleep(5 * 60)
-    bot.checkuotd()
+    try:
+        time.sleep(5 * 60)
+        bot.checkuotd()
+    except KeyboardInterrupt:
+        break
